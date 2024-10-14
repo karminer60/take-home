3aacc030bd92651590b39ca297e25a0cd9e5f4b0
karminer60 <karminer60@gmail.com>
2024-10-13 20:39:40 -0700

    sets up node test framework

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
 const assert = require('node:assert/strict');
+const { describe, it, before, after, beforeEach, afterEach } = require("node:test");
 
-async function sortHackerNewsArticles() {
-  // launch browser
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  const page = await context.newPage();
+describe("Hacker News Articles", () => {
+  let browser;
+  let context;
+  before(async () => {
+    // launch browser
+    browser = await chromium.launch({ headless: false });
+  });
+  beforeEach(async () => {
+    context = await browser.newContext();
+  });
+  afterEach(async () => {
+    await context.close();
+  });
+  after(async () => {
+    await browser.close();
+  });
+  it("are sorted", async () => {
+    const page = await context.newPage();
+  
+    // go to Hacker News
+    await page.goto("https://news.ycombinator.com/newest");
+  
+    //get all table's age attributes
+    const listItems = await page.$$('.age')
+  
+    //extract the title content of each list item
+    const titles = await Promise.all(listItems.map(async(item) => await item.getAttribute('title')));
+  
+    //Create a copy of the list, sorted
+    const sortedTitles = titles.toSorted().reverse();
+  
+    //Compare tiles to sortedTitles
+    assert.deepEqual(titles, sortedTitles);
+  });
+});
 
-  // go to Hacker News
-  await page.goto("https://news.ycombinator.com/newest");
-
-  //get all table's age attributes
-  const listItems = await page.$$('.age')
-
-  //extract the title content of each list item
-  const titles = await Promise.all(listItems.map(async(item) => await item.getAttribute('title')));
-
-  //Create a copy of the list, sorted
-  const sortedTitles = titles.toSorted();
-
-  //Compare tiles to sortedTitles
-  assert.deepEqual(titles, sortedTitles);
-}
-(async () => {
-  await sortHackerNewsArticles();
-})();
