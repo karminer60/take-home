7b3e062bb257b1cfdf4917a9792d113861d6d17d
karminer60 <karminer60@gmail.com>
2024-10-13 19:13:48 -0700

    uses node assert to check that two lists match

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
+const assert = require('node:assert/strict');
 
 async function sortHackerNewsArticles() {
   // launch browser
@@ -9,7 +10,6 @@ async function sortHackerNewsArticles() {
 
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
-}
 
   //get all table's age attributes
   const listItems = await page.$$('.age')
@@ -17,8 +17,12 @@ async function sortHackerNewsArticles() {
   //extract the title content of each list item
   const titles = await Promise.all(listItems.map(async(item) => await item.getAttribute('title')));
 
+  //Create a copy of the list, sorted
+  const sortedTitles = titles.toSorted();
 
-
+  //Compare tiles to sortedTitles
+  assert.deepEqual(titles, sortedTitles);
+}
 (async () => {
   await sortHackerNewsArticles();
 })();
