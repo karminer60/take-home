e11955765996585262bd3d4a01041d2f597d7e5a
karminer60 <karminer60@gmail.com>
2024-10-17 18:21:52 -0700

    adds while loop, to click on morelink, and add up to 100 dates to titles list

--- index.js
+++ index.js
@@ -24,18 +24,23 @@ describe("Hacker News Articles", () => {
   
     //go to Hacker News
     await page.goto("https://news.ycombinator.com/newest");
-  
-    //get all table's age attributes
-    const listItems = await page.$$('.age')
-  
-    //extract the title content of each list item
+    let listItems = await page.$$('.age')
     const titles = await Promise.all(listItems.map(async(item) => await item.getAttribute('title')));
+    while(titles.length < 100){
+      // await page.$$('.morelink').click();
+      //get all table's age attributes
+      listItems = await page.$$('.age')
+  
+      //extract the title content of each list item
+      titles.push(...await Promise.all(listItems.map(async(item) => await item.getAttribute('title'))));
+    }
   
     //create a copy of the list, sorted
     const sortedTitles = titles.toSorted().reverse();
   
     //compare titles to sortedTitles
     assert.deepEqual(titles, sortedTitles);
+  
   });
 });
 
